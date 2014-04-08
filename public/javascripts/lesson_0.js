302e137d087892524632cc603f5656f4655ccf53
Admiral Potato <admiral@nuclearpixel.com>
2014-04-08 00:26:24 -0600

    Lesson 0: adding bulk shipping options

--- public/javascripts/lesson_0.js
+++ public/javascripts/lesson_0.js
@@ -8,6 +8,16 @@ var shippingMethodList = [
 		price: 10
 	},
 	{
+		name: 'Bulk Goats (2 business days, 2+ goats only)',
+		price: 2,
+		bulkRequirement: 2
+	},
+	{
+		name: 'Ultra Bulk Goats (2 business days, 4+ goats only)',
+		price: 0,
+		bulkRequirement: 4
+	},
+	{
 		name: 'Super Goat (1 business day)',
 		price: 100
 	}
@@ -25,7 +35,7 @@ var Item = function(){
 
 var GoatShippersViewModel = function(){
 	var t = this;
-	t.itemList = ko.observableArray([]);
+	t.itemList = ko.observableArray([new Item()]);
 	t.addItem = function(){
 		t.itemList.push(new Item());
 	};
@@ -39,6 +49,30 @@ var GoatShippersViewModel = function(){
 			total += item.shippingMethod().price;
 		});
 		return total.toFixed(2);
+	});
+	t.bulkRequirementsMet = ko.computed(function(){
+		var requirementMap = {},
+			requirementName,
+			requirementValue,
+			result = true;
+		t.itemList().forEach(function(item){
+			var method = item.shippingMethod();
+			if(method.bulkRequirement){
+				if(!requirementMap.hasOwnProperty(method.name)){
+					requirementMap[method.name] = method.bulkRequirement;
+				}
+				requirementMap[method.name] -= 1;
+			}
+		});
+		for(requirementName in requirementMap){
+			if(requirementMap.hasOwnProperty(requirementName)){
+				requirementValue = requirementMap[requirementName];
+				if(requirementValue > 0){
+					result = false;
+				}
+			}
+		}
+		return result;
 	});
 };
 
